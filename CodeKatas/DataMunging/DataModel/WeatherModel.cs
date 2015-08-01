6ce768b54a8b41b949491f54c37e04acd7ac527f
Jacob Estell <jacob.estell@gmail.com>
2015-08-01 12:19:55 -0400

    Working Weather Code Kata

---
+++ CodeKatas/DataMunging/DataModel/WeatherModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMunging.DataModel
+{
+    class WeatherModel
+    {
+        public int Day { get; set; }
+
+        public int MaxTemp { get; set; }
+
+        public int MinTemp { get; set; }
+
+        public int TempSpread { get { return CalculateMaxSpread(MaxTemp, MinTemp); } }
+
+        public static int CalculateMaxSpread(int maxTemp, int minTemp)
+        {
+            return maxTemp - minTemp;
+        }
+    }
+}
--- CodeKatas/DataMunging/Program.cs
+++ CodeKatas/DataMunging/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using DataMunging.DataModel;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace DataMunging
 {
@@ -10,6 +11,62 @@ namespace DataMunging
     {
         static void Main(string[] args)
         {
+            WriteWeatherToScreen(GetWeatherWithHighestSpread(ReadFile()));
+            Console.ReadLine();
+        }
+
+        static List<WeatherModel> ReadFile()
+        {
+            string executableLocation = Path.GetDirectoryName(
+                Assembly.GetExecutingAssembly().Location);
+
+            string dataFileLocation = Path.Combine(executableLocation, "weather.dat");
+
+            var weatherModels = new List<WeatherModel>();
+
+            using (StreamReader file = new StreamReader(dataFileLocation))
+            {
+                string line;
+                int counter = 0;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrWhiteSpace(line) && counter != 0 && counter != 32)
+                    {
+                        weatherModels.Add(new WeatherModel
+                        {
+                            Day = ConvertBadStringToInt(line.Substring(0, 4)),
+                            MaxTemp = ConvertBadStringToInt(line.Substring(4, 6)),
+                            MinTemp = ConvertBadStringToInt(line.Substring(10, 6))
+                        });
+                    }
+
+                    counter++;
+                }
+            }
+
+            return weatherModels;
+        }
+
+        static WeatherModel GetWeatherWithHighestSpread(List<WeatherModel> weatherModels)
+        {
+            return weatherModels.OrderByDescending(x => x.TempSpread).FirstOrDefault();
+        }
+
+        static void WriteWeatherToScreen(WeatherModel weatherModel)
+        {
+            Console.WriteLine($"Largest Spread Day {weatherModel.Day.ToString()}, Spread {weatherModel.TempSpread.ToString()}");
+        }
+
+        static int ConvertBadStringToInt(string tempString)
+        {
+            int temp;
+            if (!int.TryParse(tempString.Trim(), out temp))
+            {
+                if (!int.TryParse(tempString.Replace("*", "").Trim(), out temp))
+                    throw new ArgumentException("I Don't even");
+            }
+
+            return temp;
         }
     }
 }
